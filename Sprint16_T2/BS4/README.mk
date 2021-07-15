322729b48fb564121734b9ff4721f110c56b59c5
Luis Martin <luis.martin.de.pablo@gmail.com>
2021-07-15 16:36:30 +0200

    Update README.mk

--- Sprint16_T2/BS4/README.mk
+++ Sprint16_T2/BS4/README.mk
@@ -1,1 +1,7 @@
+# Web Scraping Project with BeautifulSoup and Selenium
 
+This notebook imports the packages:
+  - requests
+  - BeautifulSoup
+  - selenium
+ To extract data from the web https://www.bolsamadrid.es
